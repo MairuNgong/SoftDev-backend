2de495d637a2346fe6856e24bd70f9a99f90b1bd
jp <ssupawej@gmail.com>
2025-09-01 20:57:19 +0700

    Implement Google OAuth authentication in the server

--- server/server.js
+++ server/server.js
@@ -1,24 +1,88 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const sequelize = require('./db');
+const passport = require('passport');
+const session = require('express-session');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('./models/User');
 
 dotenv.config();
 const app = express();
 
+
+app.use(session({
+  secret: 'your_secret_key',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user.email);
+});
+
+passport.deserializeUser(async (email, done) => {
+  try {
+    const user = await User.findByPk(email);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
+},async (accessToken, refreshToken, profile, done) => {
+  try {
+    const email = profile.emails[0].value;
+    const name = profile.displayName;
+
+    let user = await User.findByPk(email);
+    if (!user) {
+      user = await User.create({ email, name });
+    }
+    console.log("User from DB:", user);
+    done(null, user);
+  }
+  catch (err) {
+    done(err, null);
+  }
+}));
+
+
+//Routes
+
 app.use(express.json());
 
-// Root route
-app.get('/', (req, res) => res.send('Express + Postgres + Sequelize + Docker 🚀'));
+app.get("/", (req, res) => {
+  res.send(`<a href="/auth/google">Login with Google</a>`);
+});
 
-// Create
-app.post('/users', async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    res.send(`Hello ${req.user.name} (${req.user.email})`);
   }
+);
+
+app.get("/profile", (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).send('You are not authenticated');
+  }
+  res.send(`Hello ${req.user.name} (${req.user.email})`);
+});
+
+app.get("/logout", (req, res) => {
+  req.logout(() => {
+    res.redirect("/");
+  });
 });
 
 // Read all
