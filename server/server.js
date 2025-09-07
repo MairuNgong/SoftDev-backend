987afeb2408e4c8f4bef9364eec7b51c0392b153
Tanawat Sawangwong <kinggotothemoon@gmail.com>
2025-09-07 19:05:24 +0700

    adjust file

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const sequelize = require('./config/db');
-const passport = require('passport');
-const session = require('express-session');
+const passport = require('./server/config/passport'); // ← FIXED: Import from config folderconst session = require('express-session');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const jwt = require('jsonwebtoken');
 const User = require('./models/User');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
 
 dotenv.config();
 const app = express();
@@ -20,45 +21,17 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// passport.serializeUser((user, done) => {
-//   done(null, user.email);
-// });
 
-// passport.deserializeUser(async (email, done) => {
-//   try {
-//     const user = await User.findByPk(email);
-//     done(null, user);
-//   } catch (err) {
-//     done(err, null);
-//   }
-// });
-
-// passport.use(new GoogleStrategy({
-//   clientID: process.env.GOOGLE_CLIENT_ID,
-//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   callbackURL: process.env.GOOGLE_CALLBACK_URL
-// },async (accessToken, refreshToken, profile, done) => {
-//   try {
-//     const email = profile.emails[0].value;
-//     const name = profile.displayName;
-
-//     let user = await User.findByPk(email);
-//     if (!user) {
-//       user = await User.create({ email, name });
-//     }
-//     console.log("User from DB:", user);
-//     done(null, user);
-//   }
-//   catch (err) {
-//     done(err, null);
-//   }
-// }));
 
 
 //Routes
 
 app.use(express.json());
 
+
+
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 // app.get("/", (req, res) => {
 //   res.send(`<a href="/auth/google">Login with Google</a>`);
 // });
